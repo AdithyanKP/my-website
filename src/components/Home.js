89a25f0053264ff5574367df92834435fc559ff8
Adithyan k P <adithyankp777@gmail.com>
2022-03-01 14:19:00 +0530

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
-import { Avatar, Link } from "@mui/material";
+import { Avatar } from "@mui/material";
 import image from "../assets/image.jpeg";
 
 const Home = () => {
