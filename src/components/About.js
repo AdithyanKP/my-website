dcbf551cf926fbcb15126429376eb4b208eff428
Adithyan k P <adithyankp777@gmail.com>
2022-03-01 15:19:39 +0530

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import styles from "./about.module.css";
 const About = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.text}>React Js ✔</h1>
+      <h1 className={styles.text}>React Native ✔</h1>
+      <h1 className={styles.text}>Node Js ✔</h1>
+      <h1 className={styles.text}>Git ✔</h1>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -20,13 +20,17 @@ const Home = () => {
       <div className={styles.linkContainer}>
         <h1 className={styles.linkText}>Social Links</h1>
         <div className={styles.link}>
-          <a href="https://www.linkedin.com/in/adithyan-k-p/">Visit LinkedIn</a>
+          <a href="https://www.linkedin.com/in/adithyan-k-p/">
+            Visit LinkedIn ✔
+          </a>
         </div>
         <div className={styles.link}>
-          <a href="https://github.com/AdithyanKP">Visit Github</a>
+          <a href="https://github.com/AdithyanKP">Visit Github ✔</a>
         </div>
         <div className={styles.link}>
-          <a href="https://www.instagram.com/adhithyankp_/">Visit Instagram</a>
+          <a href="https://www.instagram.com/adhithyankp_/">
+            Visit Instagram ✔
+          </a>
         </div>
       </div>
     </div>
