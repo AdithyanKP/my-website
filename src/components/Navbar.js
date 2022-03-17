d0111c73a559882583eedbc40fa5145f71310b6c
Adithyan k P <adithyankp777@gmail.com>
2022-03-17 17:27:46 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,8 +17,8 @@ const Navibar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="skills">Skills</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/skills">Skills</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
