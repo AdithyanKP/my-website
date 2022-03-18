4c61e10212c3bbef27e214dc3313e7684ed74f74
Adithyan k P <adithyankp777@gmail.com>
2022-03-18 11:57:13 +0530

    updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,8 @@ import React from "react";
 import image from "../../assets/pic.png";
 import netfliximage from "../../assets/netflix.png";
 import eplant from "../../assets/eplant.png";
-
+import quiz from "../../assets/quiz.png";
+import todo from "../../assets/todo.png";
 import styles from "./Projects.module.css";
 import { Button } from "react-bootstrap";
 
@@ -10,22 +11,25 @@ const Projects = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>Projects</h1>
-      <div className={styles.imagecontainer}>
-        <div className={styles.imagecontainer}>
-          <p className={styles.title}>E-Plant</p>
 
-          <div>
-            <img className={styles.image} src={eplant} alt=""></img>
-          </div>
-          <div className={styles.a}>
-            <Button
-              variant="info"
-              href="https://github.com/AdithyanKP/E-PLANT.git"
-            >
-              View Github repo
-            </Button>
-          </div>
+      {/*    E-plant project */}
+      <div className={styles.imagecontainer}>
+        <p className={styles.title}>E-Plant</p>
+        <div>
+          <img className={styles.image} src={eplant} alt=""></img>
         </div>
+        <div className={styles.a}>
+          <Button
+            variant="info"
+            href="https://github.com/AdithyanKP/E-PLANT.git"
+          >
+            View Github repo
+          </Button>
+        </div>
+      </div>
+
+      {/* covid -19 Tracker */}
+      <div className={styles.imagecontainer}>
         <p className={styles.title}>Covid-19 tracker</p>
         <div>
           <img className={styles.image} src={image} alt=""></img>
@@ -40,6 +44,8 @@ const Projects = () => {
           </Button>
         </div>
       </div>
+
+      {/* Netflix-clone */}
       <div className={styles.imagecontainer}>
         <p className={styles.title}>Netflix clone</p>
         <div>
@@ -48,6 +54,34 @@ const Projects = () => {
 
         <div className={styles.a}>
           <Button variant="info" href="https://adiflix.netlify.app/">
+            View Demo
+          </Button>
+        </div>
+      </div>
+
+      {/*  Quiz-app */}
+      <div className={styles.imagecontainer}>
+        <p className={styles.title}>Quiz-App</p>
+        <div>
+          <img className={styles.image} src={quiz} alt=""></img>
+        </div>
+
+        <div className={styles.a}>
+          <Button variant="info" href="https://quizz-kp.netlify.app/">
+            View Demo
+          </Button>
+        </div>
+      </div>
+
+      {/* Todo app */}
+      <div className={styles.imagecontainer}>
+        <p className={styles.title}>Todo App</p>
+        <div>
+          <img className={styles.image} src={todo} alt=""></img>
+        </div>
+
+        <div className={styles.a}>
+          <Button variant="info" href="https://todo-app-adithyan.netlify.app/">
             View Demo
           </Button>
         </div>
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -5,6 +5,7 @@ const Skill = () => {
     <div className={styles.container}>
       <h1 className={styles.text}>React Js ✔</h1>
       <h1 className={styles.text}>React Native ✔</h1>
+      <h1 className={styles.text}>Redux js ✔</h1>
       <h1 className={styles.text}>Node Js ✔</h1>
       <h1 className={styles.text}>Javascript ✔</h1>
       <h1 className={styles.text}>Git ✔</h1>
