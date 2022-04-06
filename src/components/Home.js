1e87ab17533569c8bfb11befcd2884dfbda600f7
Adithyan k P <adithyankp777@gmail.com>
2022-04-06 10:29:55 +0530

    animation added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,30 @@ import styles from "./Home.module.css";
 import { Avatar } from "@mui/material";
 import image from "../assets/image.jpeg";
 import Projects from "./Projects/Projects";
+import animation from "../assets/animation.gif";
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}>
-        <Avatar alt="Remy Sharp" src={image} sx={{ width: 70, height: 70 }} />
+      <div className={styles.layoutOne}>
+        <div className={styles.miniLayoutOne}>
+          <div className={styles.image}>
+            <Avatar
+              alt="Remy Sharp"
+              src={image}
+              sx={{ width: 70, height: 70 }}
+            />
+          </div>
+          <p className={styles.para}>
+            Hello I am Adithyan K P I currently work as a software developer at
+            Infospica consultancy and services💼. I've completed my BCA
+            graduation in 2018-2021🎓. Currently working in custom web and
+            mobile application development.
+          </p>
+        </div>
+        <img className={styles.animation} src={animation} alt=""></img>
       </div>
 
-      <p className={styles.para}>
-        Hello I am Adithyan K P I currently work as a software developer at
-        Infospica consultancy and services💼. I've completed my BCA graduation
-        in 2018-2021🎓. <br />
-        Currently working in custom web and mobile application development.
-      </p>
       <Projects />
     </div>
   );
