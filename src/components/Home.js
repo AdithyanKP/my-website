bda9a7fab97fe0d1eb7489820b76b47e97a7387b
Adithyan k P <adithyankp777@gmail.com>
2022-04-17 16:44:37 +0530

    updation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,18 @@ import React from "react";
 import styles from "./Home.module.css";
 
 import Projects from "./Projects/Projects";
-import animation from "../assets/adi.gif";
+import animation from "../assets/developer.gif";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.layoutOne}>
         <div className={styles.miniLayoutOne}>
-          {/* <div className={styles.image}>
-            <Avatar
-              alt="Remy Sharp"
-              src={image}
-              sx={{ width: 120, height: 120 }}
-            />
-          </div> */}
           <p className={styles.para}>
-            Hi there... I currently work as a software developer at Infospica
-            consultancy and services💼. I've completed my BCA Graduation in
-            2018-2021🎓. Currently working in custom web and mobile application
+            Hi there..
+            <br /> I currently work as a software developer at Infospica
+            consultancy and services. I've completed my BCA Graduation in
+            2018-2021. Currently working in custom web and mobile application
             development.
           </p>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,17 @@ import React from "react";
 import { Nav, Container, Navbar } from "react-bootstrap";
 const Navibar = () => {
   return (
-    <Navbar style={{ backgroundColor: "white" }} expand="lg" sticky="top">
+    <Navbar style={{ backgroundColor: "#03242b" }} expand="lg" sticky="top">
       <Container>
         <Navbar.Brand href="/">
-          <h3>ADITHYAN</h3>
+          <h3 style={{ color: "#7b7b7b" }}>ADITHYAN</h3>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/skills">Skills</Nav.Link>
+            <Nav.Link href="/skills" style={{ color: "#7b7b7b" }}>
+              Skills
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
